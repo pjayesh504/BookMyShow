5e26406d596cc5404715a39ef5b6095ac6964c10
pjayesh504 <pjayesh504@gmail.com>
2024-08-30 00:44:13 +0530

    add register route

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const dburl = "mongodb://pjayesh504:Jayesh12345@ookMyShow/?ssl=true&replicaSet=atlas-xfdlxa-shard-0&authSource=admin&retryWrites=true&w=majority&appName=BookMyShow"
+const userRoutes = require('./routes/userRoutes');
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+const dburl = "mongodb+srv://pjayesh504:Jayesh12345@bookmyshow.6lr6e.mongodb.net/BookMyShow?retryWrites=true&w=majority&appName=BookMyShow";
+
+app.use(express.json());
+
 
 mongoose.connect(dburl).then(() => {
   console.log('Connected to MongoDB');
 }).catch((err) => {
   console.error('Error connecting to MongoDB:', err);
+});
+
+
+app.use('/api/users',userRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
+// const authMiddleware = require("../middlewares/authMiddleware");
+
+
+router.post('/register', async (req, res) => {
+    try {
+        const userExists = await User.findOne({ email: req.body.email });
+
+        if (userExists) {
+            res.send({
+                success: false,
+                message: "User already exists"
+            });
+            return;
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+
+        const newUser = await User(req.body);
+        await newUser.save();
+
+        res.send({
+            success: true,
+            message: "user created successfully"
+        });
+    } catch (err) {
+        console.log(err);
+        res.send({
+            success: false,
+            message: err
+        })
+    }
+})
+
+
+module.exports = router;
